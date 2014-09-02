44b7cadec5f13a36be696acc11d16f05f23bd2d1
Diane Douglas & lee kebler <student@epicodus.com>
2014-09-02 15:04:27 -0700

    Add crud for lines model and 'new' links for both lines and stations.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,24 +1,47 @@
 class LinesController <ApplicationController
 
   def index
+    @lines = Line.all
   end
 
   def new
+    @line = Line.new()
   end
 
   def create
+    @line = Line.new(line_params)
+    if @line.save
+      flash[:notice] = "Line Creation Success-ation"
+      redirect_to "/lines"
+    else
+      flash[:notice] = "once again you have failed me."
+      render "new"
+    end
   end
 
   def edit
+    @line = Line.find(params[:id])
   end
 
   def show
+    @line = Line.find(params[:id])
   end
 
   def update
+    @line = Line.find(params[:id])
+    @line.update(line_params)
+    redirect_to "/lines"
   end
 
   def destroy
+    @line = Line.find(params[:id])
+    @line.destroy
+    redirect_to "/lines"
+  end
+
+private
+  def line_params
+  params.require(:line).permit(:name)
   end
 
 end
