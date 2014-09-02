e40ea1a5ed2117db6f5194fe0f1469ef33a29c57
Diane Douglas & lee kebler <student@epicodus.com>
2014-09-02 15:32:34 -0700

    Add crud for stops.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,24 +1,46 @@
 class StopsController <ApplicationController
 
   def index
+    @stops = Stop.all
   end
 
   def new
+    @stop = Stop.new()
   end
 
   def create
+    @stop = Stop.new(stop_params)
+    if @stop.save
+      flash[:notice] = "Stop Creation Success-ation"
+      redirect_to "/stops"
+    else
+      flash[:notice] = "once again you have failed me."
+      render "new"
+    end
   end
 
   def edit
+    @stop = Stop.find(params[:id])
   end
 
   def show
+    @stop = Stop.find(params[:id])
   end
 
   def update
+    @stop = Stop.find(params[:id])
+    @stop.update(stop_params)
+    redirect_to "/stops"
   end
 
   def destroy
+    @stop = Stop.find(params[:id])
+    @stop.destroy
+    redirect_to "/stops"
   end
 
+private
+  def stop_params
+  params.require(:stop).permit(:station_id, :line_id)
+  end
 end
