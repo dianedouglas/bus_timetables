a1eea2aa9cb499b423f4c9677630d5d134e03fbc
Diane Douglas & lee kebler <student@epicodus.com>
2014-09-02 11:56:06 -0700

    Add activerecord associations for station, stop, line models.

---
+++ spec/station_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+describe Station do
+
+  it { should validate_presence_of :name }
+  it { should have_many(:lines).through(:stops)}
+
+end
---
+++ spec/stop_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+describe Stop do
+
+  it { should belong_to :station}
+  it { should belong_to :line}
+
+end
